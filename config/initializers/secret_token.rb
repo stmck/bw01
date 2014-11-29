07fef4016a4f82f530d3f2657ff5ea1e8e0175ef
michiko <sato@michiko-no-MacBook-Pro.local>
2014-11-29 20:03:28 +0900

    01

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterBootswatchRailsDemo::Application.config.secret_key_base = '59e4c69cee958f5f8046c742ec614317f37fd2d1545b9063fc5f1c472b464246dfd9615e0437072e5eb962b7eb13399321ece2dbb4c1cf6d03feb7a6951fe124'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwitterBootswatchRailsDemo::Application.config.session_store :cookie_store, key: '_twitter-bootswatch-rails-demo_session'
